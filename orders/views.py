ee39fbbe7dbd4f8fc977c07018a052eaa44fe7df
rakesh kumar <rakesh@binarynote.com>
2020-02-13 22:10:31 +0530

    error in orders

--- orders/views.py
+++ orders/views.py
@@ -7,14 +7,16 @@ from cart.cart import Cart
 
 class order_create(request):
     cart = Cart(request)
-    if request.method=='POST':
-        form = OrderfCreateForm(request.FORM)
+    if request.method == 'POST':
+        form = OrderfCreateForm(request.POST)
         if form.is_valid():
             order = form.save()
             for item in cart:
                 orderItem.objects.create(order=order,product =item['product'],price=item['price'],quantity=item['quantity'] )
+
             cart.clear()
             return render(request,'orders/order/created.html',{'order':order})
+    
     else:
         form = OrderfCreateForm()
         return render(request,'orders/order/creat.html',{'cart':cart,'form':form})
