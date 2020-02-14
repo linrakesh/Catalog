10e6e13aeacc4c4659f690aaa026e80695df186f
rakesh <rakesh.linux@gmail.com>
2020-02-14 09:35:51 +0530

    shop

--- Catalog/settings.py
+++ Catalog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'cart',
 
 ]
 
@@ -115,6 +116,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+# cart session id
+CART_SESSION_ID = 'cart'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
